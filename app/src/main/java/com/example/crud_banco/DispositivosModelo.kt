b321e39aa2ee166268aabe7552f34d184b95ee62
GabsGonnn <gabriel.goncalves.2006@gmail.com>
2024-04-23 18:20:50 -0300

    Insertion

---
+++ app/src/main/java/com/example/crud_banco/DispositivosModelo.kt
@@ -0,0 +1,13 @@
+package com.example.crud_banco
+
+import java.time.LocalDate
+
+class DispositivosModelo(
+    var dispId: String? = null,
+    var dispNome: String? = null,
+    var dispTipo: String? = null,
+    var dispStatus: String? = null,
+    var dispLocal: String? = null,
+    var dispDtInst: String? = null,
+    var dispDtAtt: String? = null
+)
---
+++ app/src/main/java/com/example/crud_banco/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.crud_banco
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnInsert = findViewById<Button>(R.id.btnInsert)
+
+        btnInsert.setOnClickListener{
+            val intent = Intent(this, InsertionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
