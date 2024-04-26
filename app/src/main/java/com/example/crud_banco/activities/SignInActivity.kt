3ca4483376104cd31e64f386d50857e86a82b24d
GabsGonnn <gabriel.goncalves.2006@gmail.com>
2024-04-26 08:43:38 -0300

    Tela de Login e Cadastro

---
+++ app/src/main/java/com/example/crud_banco/activities/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.crud_banco.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crud_banco.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
